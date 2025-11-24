c24d6a7be4104e1a2d7aecf0b9f9806d1c086c26
alraro <alfonsoramosrojas@gmail.com>
2025-11-24 12:41:56 +0100

    Implemented simple persona

--- src/Persona.java
+++ src/Persona.java
@@ -1,2 +1,59 @@
-public interface Persona {
+public abstract class Persona {
+	private String nombre;
+	private String Apellido1;
+	private String Apellido2;
+	private Integer Edad;
+
+	public Persona(String nombre, String apellido1, String apellido2, Integer edad) {
+		this.nombre = nombre;
+		Apellido1 = apellido1;
+		Apellido2 = apellido2;
+		Edad = edad;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido1() {
+		return Apellido1;
+	}
+
+	public void setApellido1(String apellido1) {
+		Apellido1 = apellido1;
+	}
+
+	public String getApellido2() {
+		return Apellido2;
+	}
+
+	public void setApellido2(String apellido2) {
+		Apellido2 = apellido2;
+	}
+
+	public Integer getEdad() {
+		return Edad;
+	}
+
+	public void setEdad(Integer edad) {
+		Edad = edad;
+	}
+
+	public String getNombreCompleto() {
+		return this.nombre + " " + this.Apellido1 + " " + this.Apellido2;
+	}
+
+	public void setNombreCompleto(String name) {
+		String[] splitted = name.split(" ");
+		if (splitted.length != 3) {
+			throw new IllegalArgumentException("Error poniendo nombre, formato: Nombre Apellido1 Apellido2");
+		}
+		setNombre(splitted[0]);
+		setNombre(splitted[1]);
+		setNombre(splitted[2]);
+	}
 }
