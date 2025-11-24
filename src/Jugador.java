667ae30e6ea9ac7fd0f01a1926feccd2ecdf460e
alraro <alfonsoramosrojas@gmail.com>
2025-11-24 13:42:40 +0100

    Temp commit

--- src/Jugador.java
+++ src/Jugador.java
@@ -1,6 +1,14 @@
+import Enums.Categorias;
+import Enums.Deportes;
+import Enums.TipoJugador;
+
 public class Jugador extends Persona{
 
-	private
+	private Deportes deporte;
+	private Equipo equipo;
+	private TipoJugador tipoJugador;
+	private Categorias categoria;
+
 
 	public Jugador(String nombre, String apellido1, String apellido2, Integer edad) {
 		super(nombre, apellido1, apellido2, edad);
